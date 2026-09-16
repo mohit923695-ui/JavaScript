8b14739fbf2003a1d8eba7e7e841d38165a4e2f4
mohit923695-ui <mohit923695@gmail.com>
2026-09-16 10:43:24 +0530

    practice  exersice

---
+++ 02_practise_exercise/main.js
@@ -0,0 +1,13 @@
+// 1. Use an alert to display Good Morning.
+alert("Good Morning");
+
+// 2. Display your name in a popup.
+alert("mohit "); // Replace "Your Name" with your actual name
+
+// 3. Using Math calculate the following:
+console.log("75 - 25 =", 75 - 25);
+console.log("3 + 3 - 5 =", 3 + 3 - 5);
+
+// 4. Change Facebook page to display "I am Learning JS"
+// Note: This specific command is meant to be executed directly in a browser console on the actual webpage.
+document.body.innerHTML = "<h1>I am Learning JS</h1>";
