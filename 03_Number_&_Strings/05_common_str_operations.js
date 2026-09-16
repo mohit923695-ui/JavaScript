85a3956ce21dc9bd28d7e7fcd9644efb202d3ed1
mohit923695-ui <mohit923695@gmail.com>
2026-09-16 11:59:02 +0530

    common str operations

---
+++ 03_Number_&_Strings/05_common_str_operations.js
@@ -0,0 +1,7 @@
+let str = "JavaScript";
+
+console.log(str.length);        // Length
+console.log(str.toUpperCase()); // JAVASCRIPT
+console.log(str.toLowerCase()); // javascript
+console.log(str.charAt(0));     // J
+console.log(str.includes("Script")); // true
