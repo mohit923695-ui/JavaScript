b9b4b5dc764f8cbd072ad3e27c47bada2f15d41e
mohit923695-ui <mohit923695@gmail.com>
2026-09-16 11:57:44 +0530

    aarthmatic operations

---
+++ 03_Number_&_Strings/01_arthmatic_operations.js
@@ -0,0 +1,11 @@
+// Arithmetic Operations in JavaScript
+
+let a = 20;
+let b = 5;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);
