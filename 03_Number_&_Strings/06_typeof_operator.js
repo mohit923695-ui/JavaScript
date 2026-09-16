9ac5b49e3356464c8fc120b383b611d4e0def933
mohit923695-ui <mohit923695@gmail.com>
2026-09-16 11:59:18 +0530

    typeof

---
+++ 03_Number_&_Strings/06_typeof_operator.js
@@ -0,0 +1,9 @@
+let name = "Mohit";
+let age = 18;
+let marks = 85.5;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof marks);
+console.log(typeof isStudent);
