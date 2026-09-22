ffcb4b6983b6dd94179ff161b24939a6421b505d
mohit923695-ui <mohit923695@gmail.com>
2026-09-22 22:42:07 +0530

    logical opt

--- 10_if-else_boolean/04_ifelse_ladder.js
+++ 10_if-else_boolean/04_ifelse_ladder.js
@@ -1,1 +1,21 @@
 // if else ladder
+
+
+
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade A+");
+}
+else if (marks >= 80) {
+    console.log("Grade A");
+}
+else if (marks >= 70) {
+    console.log("Grade B");
+}
+else if (marks >= 60) {
+    console.log("Grade C");
+}
+else {
+    console.log("Fail");
+}
---
+++ 10_if-else_boolean/06_logical_opt.js
@@ -0,0 +1,12 @@
+let age = 20;
+let student = true;
+let feePaid = false;
+
+console.log(age >= 18 && student === true);
+// true
+
+console.log(age >= 18 || feePaid === true);
+// true
+
+console.log(!student);
+// false
