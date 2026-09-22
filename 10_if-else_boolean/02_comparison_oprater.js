6cf98eec69638e542e9fb7ca3b3a2cf0f81ece19
mohit923695-ui <mohit923695@gmail.com>
2026-09-22 22:13:27 +0530

    comparison

---
+++ 10_if-else_boolean/02_comparison_oprater.js
@@ -0,0 +1,19 @@
+console.log(5==5);
+console.log(5===5);
+
+console.log(5.0==5);
+console.log(5.0===5);
+
+console.log('5.0'==5);  //true
+console.log('5.0'===5);   //false
+
+
+let a = 10;
+let b = 5;
+
+console.log(a > b);   // true
+console.log(a < b);   // false
+console.log(a == b);  // false
+console.log(a != b);  // true
+console.log(a >= 10); // true
+console.log(b <= 5);  // true
