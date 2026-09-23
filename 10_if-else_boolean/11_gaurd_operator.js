cbe6e09160990e46dd8b6002b98fb81c5b47ce85
mohit923695-ui <mohit923695@gmail.com>
2026-09-23 10:56:36 +0530

    gaurd

---
+++ 10_if-else_boolean/11_gaurd_operator.js
@@ -0,0 +1,15 @@
+// Guard Operator (&&)
+
+let age = 20;
+
+age >= 18 && console.log("You can vote");
+
+
+
+
+let username = "Mohit";
+
+username && console.log("Welcome " + username);
+
+
+
---
+++ 10_if-else_boolean/12_default_opt.js
@@ -0,0 +1,7 @@
+// Default Operator (||)
+
+let username = "";
+
+let name = username || "Guest";
+
+console.log(name);
